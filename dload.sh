3d0fb7dd58a1ea83801fa9c1a73c2d0d156d22a4
willgithub <524869004@qq.com>
2019-10-24 00:20:20 +0800

    Update dload.sh

--- dload.sh
+++ dload.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-SHELL_SCRIPT=helm.sh
+DOCKER_FILE=Dockerfile.helm
 DOCKER_HUB=willdockerhub
-TAG=$(echo $SHELL_SCRIPT | awk -F '.' '{print $1}')
+TAG=$(echo $DOCKER_FILE | awk -F '.' '{print $2}')
 
-cat > $SHELL_SCRIPT << EOF
-#!/bin/bash
-wget https://get.helm.sh/helm-${version}-linux-amd64.tar.gz
-EOF
-
-docker build -t ${DOCKER_HUB}/alpine-storage:${TAG} --build-arg SHELL_NAME=${SHELL_SCRIPT} .
+docker build -t ${DOCKER_HUB}/alpine-storage:${TAG} -f ${DOCKER_FILE} .
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push ${DOCKER_HUB}/alpine-storage:${TAG}
