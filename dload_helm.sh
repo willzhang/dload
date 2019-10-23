15c8e84e0f1cdc32aef143de7743c7caa23ed806
willgithub <524869004@qq.com>
2019-10-24 00:00:33 +0800

    Update and rename dload.sh to dload_helm.sh

---
+++ dload_helm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SHELL_SCRIPT=helm.sh
+DOCKER_HUB=willdockerhub
+TAG=$(echo $SHELL_NAME | awk -F '.' '{print $1}')
+
+cat > $SHELL_NAME << EOF
+#!/bin/bash
+wget https://get.helm.sh/helm-${version}-linux-amd64.tar.gz
+EOF
+
+docker build -t ${DOCKER_HUB}/alpine-storage:${TAG} -f ${DOCKER_FILE} --build-arg SHELL_NAME=${SHELL_SCRIPT} .
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker push ${DOCKER_HUB}/alpine-storage:${TAG}
