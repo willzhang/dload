39d934e3e083473d125f145281069d5488fb353b
willgithub <524869004@qq.com>
2019-10-23 23:17:01 +0800

    Update dload.sh

--- dload.sh
+++ dload.sh
@@ -4,7 +4,7 @@ DOCKER_HUB=willdockerhub
 DOCKER_FILE=Dockerfile.helm
 TAG=$(echo ${DOCKER_FILE} | awk -F . '{print $2}')
 
-docker build -t ${DOCKER_HUB}/alpine-storage:${TAG} -f ${DOCKER_FILE}
+docker build -t ${DOCKER_HUB}/alpine-storage:${TAG} -f ${DOCKER_FILE} .
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push ${DOCKER_HUB}/alpine-storage:${TAG}
