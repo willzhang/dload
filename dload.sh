6bd24f1c065644db863f2da9a39549b4167e4db2
willgithub <524869004@qq.com>
2019-10-24 13:27:54 +0800

    Update dload.sh

--- dload.sh
+++ dload.sh
@@ -4,6 +4,6 @@ DOCKER_FILE=Dockerfile.vagrant
 DOCKER_HUB=willdockerhub
 TAG=$(echo $DOCKER_FILE | awk -F '.' '{print $2}')
 
-docker build -t ${DOCKER_HUB}/alpine-storage:${TAG} -f ${DOCKER_FILE} .
+docker build -t ${DOCKER_HUB}/dload:${TAG} -f ${DOCKER_FILE} .
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push ${DOCKER_HUB}/alpine-storage:${TAG}
