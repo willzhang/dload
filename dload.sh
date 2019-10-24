9e3461d11dc69fa2ef612d22e0d43fe2a80028df
willgithub <524869004@qq.com>
2019-10-24 14:05:45 +0800

    Update dload.sh

--- dload.sh
+++ dload.sh
@@ -6,4 +6,4 @@ TAG=$(echo $DOCKER_FILE | awk -F '.' '{print $2}')
 
 docker build -t ${DOCKER_HUB}/dload:${TAG} -f ${DOCKER_FILE} .
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push ${DOCKER_HUB}/alpine-storage:${TAG}
+docker push ${DOCKER_HUB}/dload:${TAG}
